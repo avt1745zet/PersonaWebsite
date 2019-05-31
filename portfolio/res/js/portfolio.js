173773b73e6cdb6b2e0d13d603da6e5cc3d7ad97
Linya <avt1745zeo@gmail.com>
2019-05-31 22:33:39 +0800

    1. Update portfolio and contact page. 2. Change wrapper bg. 3. Remove scrollbar.css.

---
+++ portfolio/res/js/portfolio.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $(".workselector").mouseenter(function () {
+        $(this).children("div").stop();
+        var h = $(this).children("div").height();
+        var h2h = $(this).find("h2").height();
+        $(this).children("div").animate({ top: h / 2 - h2h / 2, bottom: h / 2 - h2h / 2, height: h2h }, "slow");
+    });
+    $(".workselector").mouseleave(function () {
+        $(this).children("div").stop();
+        $(this).children("div").animate({ top: "0", bottom: "0", height: "100%" }, "fast");
+    });
+});
