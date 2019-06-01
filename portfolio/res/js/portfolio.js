7f72548e09b1a2daaf2105ba6da79d9fd785f31a
Linya <avt1745zeo@gmail.com>
2019-06-01 15:21:33 +0800

    1. Change main page and contact page layout. 2. Change portfolio.js workselector's easing function. 3. Remove unused function in main.js.

--- portfolio/res/js/portfolio.js
+++ portfolio/res/js/portfolio.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function () {
     $(".workselector").mouseenter(function () {
         $(this).children("div").stop();
-        var h = $(this).children("div").height();
+        var h = $(this).height();
         var h2h = $(this).find("h2").height();
-        $(this).children("div").animate({ top: h / 2 - h2h / 2, bottom: h / 2 - h2h / 2, height: h2h }, "slow");
+        $(this).children("div").animate({ top: h / 2 - h2h * 0.75, bottom: h / 2 - h2h * 0.75, height: h2h * 1.5 }, 1000, "easeOutBack");
     });
     $(".workselector").mouseleave(function () {
         $(this).children("div").stop();
-        $(this).children("div").animate({ top: "0", bottom: "0", height: "100%" }, "fast");
+        $(this).children("div").animate({ top: "0", bottom: "0", height: "100%" }, 1000, "easeInBounce");
     });
 });
--- res/js/main.js
+++ res/js/main.js
@@ -1,55 +1,14 @@
 ﻿$(document).ready(function () {
-    /*ChangeBg*/
     /*
-    var imagePaths = [];
-        currentIndex = 1,
-        interval = 7000;
-
-    for (var i = 0; i < 3; i++) {
-        imagePaths[i] = './res/img/wrapperBgs/Bg_' + (i + 1) + '.jpg';
-    }
-    */
-    /*preloadImage*/
-    /*
-    var preloadImage = function (path) {
-        var imgTag = document.createElement('img');
-        imgTag.src = path;
-    }
-
-    for (var i = 0; i < imagePaths.length; i++)
-        preloadImage(imagePaths[i]);
-
-
-    function ChangeBg() {
-        $("#wrapper").css('background-image', 'url(' + imagePaths[currentIndex] + ')');
-        currentIndex = (currentIndex + 1) % imagePaths.length;
-
-        reflash();
-    }
-
-    function reflash() {
-        setTimeout(ChangeBg, interval);
-    }
-
-    setTimeout(ChangeBg, interval);
-    */
-    $('.video-content').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-
-        fixedContentPos: false
+     * Auto hide navbarCollapse1 when click anywhere or scroll page.
+     */
+    $(document).click(function () {
+        $("#navbarCollapse1").collapse('hide');
     });
-});
 
-$(document).click(function () {
-    $("#navbarCollapse1").collapse('hide');
-});
-
-$(window).scroll(function () {
-    $("#navbarCollapse1").collapse('hide');
+    $(window).scroll(function () {
+        $("#navbarCollapse1").collapse('hide');
+    });
 });
 
 
